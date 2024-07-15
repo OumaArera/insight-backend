41567392e1ab8acd623ab15a5f90ef761a3e0e02
OumaArera <johnouma999@gmail.com>
2024-07-15 16:58:27 +0300

    feat: enable user to login

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(200), nullable=False)
     last_name = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(20), nullable=True)
+    role = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(300), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
